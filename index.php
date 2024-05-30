d2f4ee21bdde9783770076f750d7d66a7109cb7d
Seku Jawara <sjawara@institutmvm.cat>
2024-05-30 16:03:48 +0200

    octavo  commit ejercicio 2.4

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
+
 <?php
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 include "hola.php";
-echo “Listado de personas:\n”;
+echo “Listado de personas:”
 include "persona.php";
+include “persona2.php”;
 ?>
+
