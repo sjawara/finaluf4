10d4674f11fed75b5e54035aae9324f56139e9bf
Seku Jawara <sjawara@institutmvm.cat>
2024-05-30 16:28:21 +0200

    undecimo  commit ejercicio 2.15

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-
 <?php
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 include "hola.php";
 echo “Listado de personas:”
 include "persona.php";
-include “persona2.php”;
+include "persona2.php";
+include “persona3.php”;
 ?>
-
